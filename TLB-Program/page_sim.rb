34fcb8f6205948e2446d6aac0777c70fedc625f0
Mizar <mkmohamedali84@gmail.com>
2023-05-29 21:02:40 -0500

    Fixing broken logic in bit shifting

--- TLB-Program/page_sim.rb
+++ TLB-Program/page_sim.rb
@@ -1,4 +1,45 @@
 # frozen_string_literal: true
 # Driver for TLB Simulation
+require_relative 'tlb'
 
-puts((12.to_f/13.to_f))
+#Read stdin for TLB details
+# FIXME handle non-existent file
+File.open("testinputs/input2.txt").each_with_index do |line, i|
+   line.chomp!
+   if i == 0 # Assign page dimensions
+     @addressInfo = line.split(' ')
+     @outerPg = @addressInfo[0].to_i
+     @innerPg = @addressInfo[1].to_i
+     @offset = @addressInfo[2].to_i
+
+     if @outerPg < 1 or @innerPg < 1 or @offset < 1
+       raise "ERROR: TLB address details must be positive; exiting..."
+     end
+
+     @totSize = @outerPg + @innerPg + @offset
+     if @totSize > 32
+       raise "ERROR: TLB address details exceeds 32 bits"
+     end
+   elsif i == 1 # assign
+     @physCost = line.to_i
+     if @physCost < 0
+       raise "ERROR: Memory Access cost must be non-negative"
+     end
+   elsif i == 2
+     @tlbInfo = line.split(" ")
+     @tlbCap = @tlbInfo[0].to_i
+     @tlbCost = @tlbInfo[1].to_i
+     if @tlbCost < 0 or @tlbCap < 0
+       raise "ERROR: TLB cost or size details invalid"
+     end
+     @tlb = TLB.new(@outerPg, @innerPg, @offset, @physCost, @tlbCost, @tlbCap) # generate TLB
+   else # For remaining input (Addresses), parse and find cost
+     address = line.to_i
+     if address < 0
+       raise "ERROR: address input may not be negative"
+     end
+     @tlb.accessMemory(address) # FIXME find out numberformatexception handling for non-numerical input
+                                # FIXME handle other exceptions not including the above
+   end
+end
+@tlb.outputTlbAccess()
--- TLB-Program/tlb.rb
+++ TLB-Program/tlb.rb
@@ -22,26 +22,87 @@ class TLB
     @@physCost = physCost
     @@tlbCost = tlbCost
     @@tlbCap = tlbCap
+
+    @@tlbTable     = []
+    @@outerPageTbl = Array.new((2 ** outerPg), -1)
+    @@innerPageTbl = Array.new((2 ** innerPg), -1)
+    @@physMem = Array.new((2 ** offset), -1)
   end
   
   def accessMemory(address)
-    if(pageAddress(address).length != 3)
-      puts('ERROR: address length does not correspond to inner/outer paging dimensions; exiting...'
-      SystemExit(2)
+    if pageAddress(address).length != 3
+      puts 'ERROR: address length does not correspond to inner/outer paging dimensions; exiting...'
+      exit 2
     end
     storeLRU(searchTlb(address), address)
   end
 
   def pageAddress(address)
-    return []
+    @out, @in = address, @offset = address
+    @tot = @@outerPgSize + @@innerPgSize + @@offsetSize
+
+    @possibleAddrs = (2 ** @tot) - 1 # possible address is (2^tot)-1; fail if over this value
+    if @possibleAddrs < address
+      raise "ERROR: this address cannot be represented using the address args passed; exiting..."
+    end
+    #input file for out, in, offset is in following format: X Y Z <X=out, Y=in, Z=offset>
+    @out = address >> (@@innerPgSize + @@offsetSize) # Derive Outer pg from first few bits (X of input)
+
+    x = @tot-1
+    y = @tot-@@outerPgSize
+    z = (x...y) # define range
+    (z.first).downto(z.last).each{ |i| # Derive Inner pg value from center few bits (Y of input)
+      @in = (@in & ~(1 << i))
+    }
+    @in = @in >> @@offsetSize
+
+    x = @tot-1
+    y = @tot-@@outerPgSize-@@innerPgSize
+    z = (x...y) # define range
+    (z.first).downto(z.last).each{ |i|
+      @offset = @offset & ~(1 << i)
+    }
+    [@out, @in, @offset] # return <out/in/offset individual values>
   end
 
   def searchTlb(address)
-
+    @i = 0
+    for @i in 0...@@tlbTable.length
+      @a = pageAddress(address) # find page table values to search for
+      @b = pageAddress(@@tlbTable[@i]) # initialize check value
+      if @a[0] == @b[0] and @a[1] = @b[1]
+        return @i
+      end
+    end
+    -1 # return invalid index if item can't be found in TLB
   end
 
   def storeLRU(cachedNdx, address)
+    @paging = pageAddress(address)
 
+    # if we have the memory in the TLB, we may directly access the phys memFrame
+    if cachedNdx != -1
+      @@tlbTable.delete_at(cachedNdx)
+      @@tlbTable.push(address)
+      @@accessTime += @@tlbCost
+    else # remove least recently used item from TLB and add recent address to the end (most recent)
+      if @@tlbTable.length == @@tlbCap
+        @@tlbTable.delete_at(0)
+      end
+        @@tlbTable.push(address)
+        @@accessTime += @@tlbCost
+
+        # simulate r/w from outer pg table
+        @@outerPageTbl[@paging[0]] = @paging[1]
+        @@accessTime += @@physCost
+
+        # simulate r/w from inner pg table
+        @@innerPgTbl[@paging[0]] = @paging[2]
+        @@accessTime += @@physCost
+    end
+    # For debugging, see paging of phys mem
+    @@accessTime += @@physCost
+    @@numAccesses += 1
   end
 
   def outputTlbAccess()
